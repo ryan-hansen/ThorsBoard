ef85f4010ae3d553b7596500096ff06ff91ddb35
Ryan Hansen <mrhattyhat@gmail.com>
2015-09-01 20:02:37 -0600

    Updated db creds

--- project/prod_settings.py
+++ project/prod_settings.py
@@ -5,7 +5,7 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'newthor',                      # Or path to database file if using sqlite3.
+        'NAME': 'thorsboard',                      # Or path to database file if using sqlite3.
         'USER': 'thordb',                       # Not used with sqlite3.
         'PASSWORD': 'n3w.th0r',                 # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
