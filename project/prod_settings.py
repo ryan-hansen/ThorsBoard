7ad90bf5d2b0ee00bcc84e3e43a5c18272053e34
Ryan Hansen <mrhattyhat@gmail.com>
2015-09-01 20:07:15 -0600

    Last time?

--- project/prod_settings.py
+++ project/prod_settings.py
@@ -5,9 +5,9 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'thorsboarddb',                      # Or path to database file if using sqlite3.
-        'USER': 'I@mB0ard!',                       # Not used with sqlite3.
-        'PASSWORD': 'n3w.th0r',                 # Not used with sqlite3.
+        'NAME': 'thorsboard',                      # Or path to database file if using sqlite3.
+        'USER': 'thorsboarddb',                       # Not used with sqlite3.
+        'PASSWORD': 'I@mB0ard!',                 # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
